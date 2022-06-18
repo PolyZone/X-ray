51e9767aeb341bee5c6a8e8da0a3df7cd4405c0c
FrederikSartovOlsen <99969953+FrederikSartovOlsen@users.noreply.github.com>
2022-06-18 18:06:35 +0200

    Add files via upload

--- plots.py
+++ plots.py
@@ -1,25 +1,28 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
+import torch
+import numpy as np
+#import McNemar from class
 
 
 
-def loss_plots(train_loss, valid_loss):
+def loss_plots(train_loss, valid_loss,title="Training and validation loss"):
     plt.plot(train_loss, label="Training loss")
     plt.plot(valid_loss, label="Validation loss")
     plt.legend()
 
-    plt.title('Training and validation loss \n With dropout-layers and lr=0.0001 og L2=0.01')
+    plt.title(title)
     plt.xlabel("Epochs")
     plt.ylabel("Loss")
     return plt.show()
 
-def acc_plots(train_acc, valid_acc):
+def acc_plots(train_acc, valid_acc, title="Training and validation accuracy"):
     plt.plot(train_acc, label="Training accuracy")
     plt.plot(valid_acc, label="Validation accuracy")
     plt.legend()
 
-    plt.title('Training and validation accuracy \n With dropout-layers and lr=0.0001 og L2=0.01')
+    plt.title(title)
     plt.xlabel("Epochs")
     plt.ylabel("Accuracy")
     return plt.show()
@@ -38,3 +41,43 @@ def plot_confusion_matrix(y_label, y_pred, title = "Confusion Matrix"):
     return plt.show()
 # https://stackoverflow.com/questions/19233771/sklearn-plot-confusion-matrix-with-labels
 
+def McNemar_plot(all_labels, models_names_pred, total_models, title="McNemar Test"):
+    import seaborn as sns
+    from matplotlib.patches import Rectangle
+    matrix = np.zeros((total_models, total_models))
+    matrix_tom = np.zeros((total_models, total_models), str)
+
+    model_name = models_names_pred
+    all_labels = torch.load(all_labels)
+    matrix = np.zeros((total_models, total_models))
+    for i in range(total_models):
+
+        pred_1 = torch.load(model_name[i])
+        for n in range(total_models):
+            pred_2 = torch.load(model_name[n])
+            p_val = testMcNemar(pred_1, pred_2, all_labels)
+            matrix[i][n] = p_val
+            if matrix[i][n] < 0.05:
+                matrix_tom[i][n] = "*"
+
+
+    labels = (np.asarray(["{0} {1:.5f}".format(matrix_tom, value)
+                          for matrix_tom, value in zip(matrix_tom.flatten(),
+                                                       matrix.flatten())])
+              ).reshape(total_models, total_models)
+
+    fig, ax = plt.subplots(figsize=(15, 10))
+
+
+    ticks_lab = ["Model " + str((i+1)) for i in (range(total_models))]
+
+    ax.set_title(title, fontsize=20);
+    sns.heatmap(matrix, annot=labels, square=True, vmin=0, vmax=0, linecolor='k', cmap="Blues_r", fmt="", ax=ax,
+                linewidth=0.5,xticklabels=ticks_lab, yticklabels=ticks_lab)
+
+    return plt.show()
+    #https://stackoverflow.com/questions/41164710/how-to-add-text-plus-value-in-python-seaborn-heatmap
+
+
+# models_names_pred = [r"model%s_final_predictions.pt" % i for i in range(69, 79)]
+# McNemar_plot("all_labels", models_names_pred, total_models=len(models_names_pred), title="McNemar Test Baseline")
